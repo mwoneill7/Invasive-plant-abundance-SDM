7d84d0dde2b7116c77c46bb80fd810cf8d2e51f9
mwoneill7 <mwn33@wildcats.unh.edu>
2017-08-18 13:02:47 -0400

    Cleaned up inefficiencies and added code for collinearity in the larger extent climate data

--- climate_data_prep.R
+++ climate_data_prep.R
@@ -79,14 +79,51 @@ for (j in 1:length(clim.list)) { ## iterate through each climate model
   print(clim.list[j]) ## keep track of model within entire loop
 }
 
-
-plot(bio.i)
-
 ##########################
 ### reading in fishnet ###
 ##########################
 fishnet = readOGR(dsn = "Fishnet", layer = "fishnet")
 plot(fishnet)
+
+
+#################################################
+### Repeat clipping with all of North America ###
+#################################################
+
+north.america <- as(extent(-170, -52, 7, 70), 'SpatialPolygons')
+#model.folder.path <- paste("climate_data", clim.list[j], sep="/") 
+## look within the climate model of the iteration, and   
+
+model.folder.path <- "climate_data/current/"
+bio.list <- list.files(model.folder.path) ## list all of the variables
+bio.list <- bio.list[1:19] ## exlude the metadata files after the 19 bioclimate variables
+
+#clipped2.model.folder.path <-paste("clipped2_climate_data", clim.list[j], sep="/")
+clipped2.model.folder.path <-paste("clipped2_climate_data", "current", sep="/")
+
+dir.create(clipped2.model.folder.path) ## creates a new folder to place the clipped data for the model of the iteration
+
+for (i in 1:length(bio.list)){ ## loop through each climate variable within the model of the iteration
+  
+  file.in <- paste(model.folder.path, bio.list[i], sep="/") 
+  ## paste together the full file name to bring it into R
+  
+  file.out <- paste(clipped2.model.folder.path, (paste(bio.list[i], "asc", sep=".")), sep="/")
+  ## create filepath for clipped data within the new folder, to export from R
+  
+  bio.i <- raster(file.in) ## read in file as raster
+  
+  bio.i <- crop(bio.i, north.america) 
+  ## crop raster to extent of united states, and then clip(mask) to the buffer
+  
+  writeRaster(bio.i, file.out, format= "ascii") ## write out raster as an ESRI grid
+  
+  print(i) ## keep track of place within model
+  
+}
+
+
+
 
 
 ##########################################
@@ -102,7 +139,7 @@ proj4string(bio.stack) <- proj4string(raster("climate_data/current/bio_1"))
 
 bio.stackM <- as.matrix(bio.stack) ## converts the stacked object into a matrix statistical analysis
 
-corr.matrix <- corr.test(as.matrix(bio.stackM), y = NULL, use = "pairwise", method="spearman", adjust="bonferroni", alpha=.05, ci=F)
+corr.matrix <- corr.test(bio.stack, y = NULL, use = "pairwise", method="spearman", adjust="bonferroni", alpha=.05, ci=F)
 ## matrix of spearman's correlation of the 19 variables, p-value adjusted using Bonferroni correction
 
 write.csv(data.frame(corr.matrix$p), "C:/Users/mwone/Google Drive/Invasive-plant-abundance-SDM-files/p.vals.csv") 
@@ -110,7 +147,35 @@ write.csv(data.frame(corr.matrix$p), "C:/Users/mwone/Google Drive/Invasive-plant
 write.csv(data.frame(corr.matrix$r), "C:/Users/mwone/Google Drive/Invasive-plant-abundance-SDM-files/corr.vals.csv") 
 ## export correlation coefficients in a table format
 
+##############################################################
+######### repeat correlations with north america data ########
+##############################################################
+
+bio.list<- list.files("clipped2_climate_data/current", full.names =T) 
+## lists all 19 climate rasters in the climate data to be used for fitting
+
+bio.stack <- stack(bio.list) ## creates an object with each of the nine rasters stacked
+proj4string(bio.stack) <- proj4string(raster("climate_data/current/bio_1")) 
+## assigns reference info based the files where these files came from 
+
+bio.stackM <- as.matrix(bio.stack) ## converts the stacked object into a matrix statistical analysis
+
+corr.matrix <- corr.test(bio.stackM, y = NULL, use = "pairwise", method="spearman", adjust="bonferroni", alpha=.05, ci=F)
+## matrix of spearman's correlation of the 19 variables, p-value adjusted using Bonferroni correction
+
+write.csv(data.frame(corr.matrix$p), "C:/Users/mwone/Google Drive/Invasive-plant-abundance-SDM-files/p.valsNA.csv") 
+## export p-values in a table format
+write.csv(data.frame(corr.matrix$r), "C:/Users/mwone/Google Drive/Invasive-plant-abundance-SDM-files/corr.valsNA.csv") 
+## export correlation coefficients in a table format
+
+
 
 ###################################################################################
+#### MESS ANALYSIS ################################################################
+###################################################################################
+
+library(dismo)
+
+
 
 
