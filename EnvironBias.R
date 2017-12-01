e369108587b152dc24314a75ebdf0df5790f89c1
mwoneill7 <mwn33@wildcats.unh.edu>
2017-12-01 16:31:43 -0500

    Ongoing updates

--- EnvironBias.R
+++ EnvironBias.R
@@ -7,13 +7,13 @@ setwd("C:/Users/mwone/Documents/geodata/")
 library(raster)
 
 ## read in thinned dataset
-edd <- read.table("C:/Users/mwone/Documents/EDDMapS data/eddmaps_thinned_09_12_2017.csv",  
+edd <- read.table("C:/Users/mwone/Documents/EDDMapS data/eddmaps_thinned_10acre.csv",  
                   header = T, sep = ",", quote= "\"", comment.char= "", stringsAsFactors = F, strip.white = T)
 
 ## read in species list
-spp <- read.table("C:/Users/mwone/Google Drive/Invasive-plant-abundance-SDM-files/SpeciesList08_28_1017.csv",  
+spp <- read.table("C:/Users/mwone/Google Drive/Invasive-plant-abundance-SDM-files/SpeciesList10_11_2017.csv",  
                   header = T, sep = ",", quote= "\"", comment.char= "", stringsAsFactors = F, strip.white = T)
-sp.list <- spp$usda.code[spp$potential.use2 == 1] ## list of useable species
+sp.list <- spp$usda.code[spp$potential.use_10ac == 1] ## list of useable species
 
 edd <- edd[edd$usda %in% sp.list,] ## subset dataset to useable species
 coordinates(edd) <- c(7,8) ## set lat/lon
@@ -38,37 +38,48 @@ edd <- cbind(edd@data, data.frame(extract(bio.stack, edd)))
 ## point data records
 head(edd)
 
-cell.ids <- unique(edd$cellID) ## list of unique cell IDs in the point data
+#     cell.ids <- unique(edd$cellID) ## list of unique cell IDs in the point data
 
-edd2 <- edd[1,] ## first row of point dataset, to initiate loop
-
-for (j in 2:length(cell.ids)){ ## loop through all cells for that species
-  cell.j <- edd[edd$cellID == cell.ids[j],][1,] ## keep 1 record from cell
-  edd2 <- rbind(edd2, cell.j) ## append the kept row to the master object
-  print(j)
-}
-
-length(edd2$id) == length(unique(edd2$cellID)) ## should be true
+#     edd2 <- edd[1,] ## first row of point dataset, to initiate loop
+#     
+#     for (j in 2:length(cell.ids)){ ## loop through all cells for that species
+#       cell.j <- edd[edd$cellID == cell.ids[j],][1,] ## keep 1 record from cell
+#       edd2 <- rbind(edd2, cell.j) ## append the kept row to the master object
+#       print(j)
+#     }
+#     
+#     length(edd2$id) == length(unique(edd2$cellID)) ## should be true
 
 ## full names of bioclimatic variables, for later titles
-fullnames <- c("Mean Diurnal Range",
-               "Max Temp of Warmest Month",
-               "Min Temp of Coldest Month",
-               "Mean Temp of Wettest Quarter",
-               "Annual Precip",
-               "Precip Seasonality")
+letters <- c("(a)", "(b)", "(c)", "(d)", "(e)", "(f)")
+full.names <- c("mean diurnal range (°C)",
+               "max temp of warmest mo. (°C)",
+               "min temp of coldest mo. (°C)",
+               "mean temp of wettest quar. (°C)",
+               "annual precipitation (mm)",
+               "precip. seasonality (mm)")
 
 bio.stackD <- as.data.frame(bio.stack)
+bio.stackD[,1:4] <- bio.stackD[,1:4]/10
+edd[,7:10] <- edd[,7:10]/10
+
+
 ## make dataframe version to make density plots
-par(mfrow=c(2,3)) 
+par(mfrow=c(2,3), cex.main=1.2, cex.lab=1.5, cex.axis=1.2, lwd=2, mar=c(5, 4, 4, 2) + 0.1) 
 for (i in 1:length(colnames(bio.stackD))){ ## loop through each bioclimatic variable
   d.i <- density(bio.stackD[,i], na.rm=T)  ## density plot for L48
   d.i2 <- density(edd[,(i+6)], na.rm=T)    ## density plot for point data
   #d.i3 <- density(edd2[,(i+6)], na.rm=T)   ## density plot for all grid cells w point data
-  plot(d.i2, col="red", main=fullnames[i], sub="")
-  #lines(d.i3, col="brown")
-  lines(d.i)
-
+  
+  if(i==1 | i==2 | i==3 | i==6){
+    plot(d.i2, col="red", main=letters[i], xlab=full.names[i], sub="")
+    #lines(d.i3, col="brown")
+    lines(d.i)
+  } else {
+    plot(d.i, col="black", main=letters[i], xlab=full.names[i], sub="")
+    #lines(d.i3, col="brown")
+    lines(d.i2, col="red")
+  }
   print(i)
 
 }
