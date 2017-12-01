ccab6e3ee24ff8e944437e70a540d5d520640c63
mwoneill7 <mwn33@wildcats.unh.edu>
2017-12-01 16:30:14 -0500

    Ongoing small updates

--- points2grids.R
+++ points2grids.R
@@ -12,8 +12,18 @@ edd <- read.table("C:/Users/mwone/Documents/EDDMapS data/eddmaps_prepped_08_22_2
                   comment.char= "", stringsAsFactors = F, strip.white = T)
 ## read in EDDMapS database (most recently prepped version)
 
-edd <- edd[edd$validInfestedArea == 1 & edd$grossAreaInAcres <= 2, ] 
+edd <- edd[edd$negative == 0 & edd$validInfestedArea == 1 & edd$ReporterFULLName != "US Army Corps of Engineers Ombil Database", ] 
 ## only include infested area data, exclude absences, exclude values over 10 acres
+
+edd <- edd[edd$grossAreaInAcres <= 1000, ]
+
+####################
+sp.list <- read.table("C:/Users/mwone/Google Drive/Invasive-plant-abundance-SDM-files/SpeciesList10_11_2017.csv", header = T, sep = ",", quote= "\"", 
+                      comment.char= "", stringsAsFactors = F, strip.white = T)
+#head(sp.list)
+sp.list <- sp.list$usda.code[sp.list$potential.use2 == 1]
+
+edd <- edd[edd$USDAcode %in% sp.list,]
 
 ## extract only the fields that are needed, to save processing time
 edd <- data.frame(cbind(1:length(edd$ScientificName), edd$ScientificName, edd$USDAcode, edd$infestedAreaInAcres, edd$negative, edd$Latitude_N, edd$Longitude_N, stringsAsFactors=F))
@@ -33,6 +43,8 @@ coordinates(edd) <- c(7,6) ## specifies long, lat
 proj4string(edd) <- CRS("+init=epsg:4326")
 #plot(edd)
 
+
+
 ## read in fishnet (square polygons corresponding to grid cells)
 fishnet <- readOGR(dsn = "fishnetPolygon", layer = "fishnetPolygon")
 ## plot(fishnet) very time consuming
@@ -51,7 +63,7 @@ tab <- over(edd, fishnet) ## overlays points and fishnet squares, yielding rows 
 tab$ptID <- as.numeric(row.names(tab)) ## convert rownames to its own field for easier use
 tab$Id <- NULL ## remove meaningless field (all zeroes)
 head(tab)
-write.csv(tab,"overlayTable2.csv", row.names=F)
+write.csv(tab,"overlayTable1000.csv", row.names=F)
 
 
 #    ############ subset fishnet to all cells containing points ############
@@ -86,6 +98,8 @@ edd$NA. <- NULL
 
 edd <- edd[!is.na(edd$cellID) & !is.na(edd$usda) & edd$usda != "",]
 ## exclude rows without a usda code or where the point did not fall in a grid cell
+#edd <- edd[edd$absence != 1,] ## remove absence data
+
 
 rm(ptsPerCell,fishnet,fishnetD,tab,cellID) ## garbage cleaning
 
@@ -116,11 +130,11 @@ for (i in 1:length(sp.list)){ ## loop through all species
     keep <- keep[1,] ## only want one record!
     edd2 <- rbind(edd2, keep) ## append the kept row to the master object
   }
-  
+#print(i)  
 }
 
 edd2 <- edd2[edd2$usda != "TEMPLATE",] ## remove template row
-write.csv(edd2, "C:/Users/mwone/Documents/EDDMapS data/eddmaps_thinned_2acre.csv", row.names=F)
+write.csv(edd2, "C:/Users/mwone/Documents/EDDMapS data/eddmaps_thinned_1000.csv", row.names=F)
 ## write out for later use
 
 head(edd2) ## where id and row.id differ
